918b6e4f02f3b8b9c98cfb4c17b61d3b2f95be78
arif <arifmr91@gmail.com>
2016-12-16 10:25:06 +0530

    format code

--- server.js
+++ server.js
@@ -1,54 +1,62 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('driverlist',['driverlist']);
+var db = mongojs('driverlist', ['driverlist']);
 var bodyParser = require('body-parser');
 
 
-
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/driverlist',function(req,res){
+app.get('/driverlist', function (req, res) {
     console.log('i recieved get request');
-    db.driverlist.find(function(err,docs){
-        console.log('docs' +JSON.stringify(docs));
+    db.driverlist.find(function (err, docs) {
+        console.log('docs' + JSON.stringify(docs));
         res.json(docs)
     })
 });
 
-app.post('/driverlist',function(req,res){
+app.post('/driverlist', function (req, res) {
     console.log('i recieved post request');
     console.log(req.body)
-    db.driverlist.insert(req.body,function(err,doc){
+    db.driverlist.insert(req.body, function (err, doc) {
         res.json(doc)
     })
 });
 
-app.delete('/driverlist/:id',function(req,res){
+app.delete('/driverlist/:id', function (req, res) {
     var id = req.params.id
     console.log(id);
-    db.driverlist.remove({_id:mongojs.ObjectId(id)},function(doc){
+    db.driverlist.remove({_id: mongojs.ObjectId(id)}, function (doc) {
         res.json(doc);
     })
 })
 
-app.get('/driverlist/:id',function(req,res){
+app.get('/driverlist/:id', function (req, res) {
     var id = req.params.id;
-    console.log('server edit id' +JSON.stringify(id));
-    db.driverlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
-        console.log('doc---------' +JSON.stringify(doc));
+    console.log('server edit id' + JSON.stringify(id));
+    db.driverlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+        console.log('doc---------' + JSON.stringify(doc));
         res.json(doc)
     })
 })
 
-app.put('/driverlist/:id',function(req,res){
-  var id = req.params.id;
+app.put('/driverlist/:id', function (req, res) {
+    var id = req.params.id;
     console.log(id);
     console.log(req.body.name);
-    db.driverlist.findAndModify({query:{_id:mongojs.ObjectId(id)},
-    update:{$set:{name:req.body.name,number:req.body.number,address:req.body.address,linces:req.body.linces}},
-    new:true},function (err, doc) {
+    db.driverlist.findAndModify({
+        query: {_id: mongojs.ObjectId(id)},
+        update: {
+            $set: {
+                name: req.body.name,
+                number: req.body.number,
+                address: req.body.address,
+                linces: req.body.linces
+            }
+        },
+        new: true
+    }, function (err, doc) {
         res.json(doc);
     })
 })
